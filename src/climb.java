7c1f54390922b6adcda9698409e6f8726a0974af
zy <ss>
2021-05-30 14:17:03 +0800

    test分支提交2

--- src/climb.java
+++ src/climb.java
@@ -2,20 +2,4 @@
 public class climb {
     public int climbStairs(int n) {
         if (n == 1) {
-            return 1;
-        }
-        int first = 1;
-        int second = 2;
-        for (int i = 3; i <= n; i++) {
-            int third = first + second;
-            first = second;
-            second = third;
-        }
-        return second;
-    }
-
-    public static void main(String[] args) {
-        climb t = new climb();
-        System.out.println(t.climbStairs(6));
-    }
-}
+            return 1;}
